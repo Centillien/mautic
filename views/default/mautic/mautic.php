594a71af40307fe7ab553c1967a40ff2878c8045
robin_conjour <rconjour@netcare.nl>
2017-01-19 12:55:57 +0100

    Updated tracking pixel to new javascript code

--- views/default/mautic/mautic.php
+++ views/default/mautic/mautic.php
@@ -1,4 +1,12 @@
 <?php
+$url = elgg_get_plugin_setting("mautic_url","mautic");
+?>
+<script>
+	(function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+		w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+			m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','<?php echo $url; ?>/mtc.js','mt');
+	<?php
 //Mautic programmatic marketing
 
 
@@ -6,7 +14,6 @@ if(!$page_title =  elgg_echo(elgg_get_context())) {
 	$page_title = $vars['config']->sitename;
 }
 
-$url = elgg_get_plugin_setting("mautic_url","mautic");
 $page_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 if($muser = elgg_get_logged_in_user_entity()) {
@@ -16,30 +23,23 @@ if($muser = elgg_get_logged_in_user_entity()) {
     $lastname = trim($name[1]);
 	$location = explode(",", $muser->location);
 	$location = $location[0];
+	?>
+	mt('send', 'pageview', {page_title: '<?php echo $page_title; ?>', page_url: '<?php echo $page_url;?>', email: '<?php echo $muser->email; ?>', firstname: '<?php echo $lastname; ?>', lastname: '<?php echo $lastname; ?>', location: '<?php echo $location; ?>', guid: '<?php echo $muser->guid; ?>'  });
+	<?php
 
-	$d = urlencode(base64_encode(serialize(array(
-    	'page_url'   => $page_url,
-    	'page_title' => $page_title,
-    	'email' => $muser->email,
-    	'firstname' => $firstname,
-    	'lastname' => $lastname,
-    	'location' => $location,
-    	'guid' => $muser->guid
-	))));
 
 } else {
 
 	if (elgg_get_plugin_setting("mautic_dont_track_anonymos","mautic") == "yes") {
 		return;
 	}
-
-	$d = urlencode(base64_encode(serialize(array(
-    	'page_url'   => $page_url,
-    	'page_title' => $page_title,
-	))));
-
+?>
+	mt('send', 'pageview');
+	<?php
 }
 
-echo '<img src="'.$url.'/mtracking.gif?d=' . $d . '" alt="" style="display: none;" />';
 
 ?>
+
+
+</script>
